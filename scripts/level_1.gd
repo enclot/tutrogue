733796d5cb051e1fa9e79a75ce509c8f51f82a72
Takanori Endo <takanori@gocco.co.jp>
2025-10-27 15:22:59 +0900

    スケルトンを追加

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -2,7 +2,7 @@ class_name Level1
 extends Node2D
 
 @onready var input_handler: MainGameInputHandler = $MainGameInputHandler
-@onready var player: Player = $Player
+@onready var player: Player = $Map/Actors/Player
 @onready var map: Map = $Map
 
 func _ready() -> void:
---
+++ scripts/skeleton.gd
@@ -0,0 +1,3 @@
+@tool
+class_name Skeleton
+extends Actor
