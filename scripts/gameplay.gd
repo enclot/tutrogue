035732b8e2fb3a9f7095c70cd9425cc65f62de7f
Takanori Endo <takanori@gocco.co.jp>
2025-10-31 14:26:17 +0900

    playerだけは常に保存して読み出すようにする

--- scripts/gameplay.gd
+++ scripts/gameplay.gd
@@ -5,7 +5,7 @@ static var instance:Gameplay
 
 var current_level:BaseLevel
 
-#var player_data:PackedScene
+var player_data:PackedScene#playrは新しいレベルに行くたびに更新していく必要があるので別に保存する
 var level_data:LevelData #レベルのパスがキーのDictionary
 
 func _ready() -> void:
@@ -30,28 +30,44 @@ func level2():
 	SceneManager.swap_scenes("res://level_2.tscn", self, current_level)
 
 func save_level_actors() -> void:
-	var level_actor_data = LevelActorData.new()
+	var level_actor_data = LevelActorData.new()#Player以外のActorが入る
 	var actor_nodes = current_level.map.get_actors_in_group("actor")
 	for actor in actor_nodes:
+		if actor is Player:
+			continue
+			
 		var actor_scene = PackedScene.new()
 		actor_scene.pack(actor)
 		level_actor_data.actor_array.append(actor_scene)		
-	
 	level_data.levels[current_level.scene_file_path] = level_actor_data.duplicate()
-
-func load_level_actors() -> void:
-	print("load_level_actors()")
 	
-	#初めてのレベルの時は読み込まない
+	var player_scene = PackedScene.new()
+	player_scene.pack(current_level.map.get_player_actor())
+	player_data = player_scene
+
+
+	
+func load_level_actors() -> void:
+	#もともとのplayerを消す
+	current_level.player.queue_free()
+	await current_level.player.tree_exited
+	
+	#playerをデータから復元
+	var loaded_player:Player = player_data.instantiate()
+	loaded_player.initialize()
+	current_level.map.actors_holder.add_child(loaded_player)
+	current_level.reset_player()
+	
+	#初めてのレベルの時はプレイヤーだけ読み込みほかのActorは初期状態のままにする
 	if not level_data.levels.has(current_level.scene_file_path):
 		return
-		
+
+	#Player以外を消す
 	var actor_nodes =  current_level.map.get_actors_in_group("actor")
 	for actor in actor_nodes:
-		actor.queue_free()
 		if actor is Player:
-			#Playerの場合シーンツリーから削除されるのを待つ
-			await actor.tree_exited
+			continue
+		actor.queue_free()
 			
 	var level_actor_data:LevelActorData = level_data.levels[current_level.scene_file_path]  
 	for actor in level_actor_data.actor_array:
