3fa0dcc989ca29b71b5ccf2d6916c51dd5e93f6c
Takanori Endo <en_tk_m@yahoo.co.jp>
2026-08-12 21:26:50 +0900

    入力を処理する

---
+++ scripts/game_input_handler.gd
@@ -0,0 +1,20 @@
+class_name GameInputHandler
+extends Node
+
+const DIRECTIONS = {
+	&"move_up": Vector2i.UP,
+	&"move_down": Vector2i.DOWN,
+	&"move_left": Vector2i.LEFT,
+	&"move_right": Vector2i.RIGHT,
+}
+
+signal action_selected(Action)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.is_pressed():
+		for action:StringName in DIRECTIONS.keys():
+			if event.is_action_pressed(action):
+				var offset: Vector2i = DIRECTIONS[action]
+				print(action, " -> ", offset)
+				# ここでアクションを作成してaction_selectedを発火させる
+				break
