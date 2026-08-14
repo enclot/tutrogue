0f1fa72c0d07f6d25f6e6ce7dfea0ce2af39756a
Takanori Endo <en_tk_m@yahoo.co.jp>
2026-08-14 16:52:33 +0900

    プレイヤーと敵を共通化する

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,10 +7,3 @@ var input_handler:GameInputHandler
 
 func get_action() -> Action:
 	return await input_handler.action_selected
-
-func move(_offset:Vector2i) -> bool:
-	var target_pos:Vector2i = grid_position +_offset
-	if map.is_walkable(target_pos):
-		grid_position = target_pos
-		return true
-	return false
