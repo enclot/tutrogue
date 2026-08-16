3a62911535f78591e83cb77529f7d23f9dc00865
Takanori Endo <en_tk_m@yahoo.co.jp>
2026-08-16 13:33:52 +0900

    HealingPosionを追加

--- scripts/healing_potion.gd
+++ scripts/healing_potion.gd
@@ -1,5 +1,12 @@
 @tool
 class_name HealingPotion extends Item
 
+func can_use(_user:Actor) -> bool:
+	var user_stats:StatsComponent = _user.get_component(StatsComponent)
+	if user_stats.hp == user_stats.max_hp:
+		print("HP is max.")
+		return false
+	return true
+
 func activate() -> Action:
 	return HealingAction.new(100)
--- scripts/item.gd
+++ scripts/item.gd
@@ -1,4 +1,5 @@
 @tool
 @abstract class_name Item extends GridObject
 
+@abstract func can_use(_user:Actor) -> bool
 @abstract func activate() -> Action
