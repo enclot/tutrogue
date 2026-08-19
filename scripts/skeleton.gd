c2b97c5909c342d24bb9c2dae0bca5fc06c51203
Takanori Endo <en_tk_m@yahoo.co.jp>
2026-08-19 12:04:07 +0900

    スケルトンの動きを修正

--- scripts/skeleton.gd
+++ scripts/skeleton.gd
@@ -2,20 +2,26 @@
 class_name Skeleton
 extends Actor
 
+#ひたすら近づく
+#隣接したら攻撃
+#Actionを返す
 func _ai_perform() -> Action:
-	#ひたすら近づく
-	#隣接したら攻撃
-	#Actionを返す
-	var target = map.get_player()
+	var target = map.get_player() as GridObject
+	#print(grid_position,  " -> target(player) ", target.grid_position )
 	var path_cells = map.get_point_path_to(grid_position,
 					target.grid_position)#自分を含む座標ででてくる
 	
+	#到達できない
+	if path_cells.is_empty():
+		return MovementAction.new(Vector2i.ZERO)
+		
 	var offset:Vector2i = target.grid_position - grid_position
 	var distance: int = abs(offset.x) + abs(offset.y)
 
 	if distance==1:#近接したら攻撃
 		return MeleeAction.new(target)		
-		
+	elif distance>5:#離れていたら動かない
+		return 	MovementAction.new(Vector2i.ZERO)
 	
 	var destination := Vector2i(path_cells[1])
 	var move_offset:Vector2i = destination - grid_position
